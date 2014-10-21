2625d63a4ac8a8efcbace4d7c860924e8a32f18c
Ryan Stack <ryanstack@Ryans-MacBook-Pro-2.local>
2014-10-20 19:23:14 -0700

    added directives folder

--- public/javascripts/client/application.js
+++ public/javascripts/client/application.js
@@ -25,30 +25,3 @@ var App = angular.module('MyApp', ['ngRoute']);
   });
 
 
-
-
-//Applciations Directives
-
-//File Reading Directive
-App.directive('onReadFile', function ($parse) {
-  return {
-    restrict: 'A',
-    scope: false,
-    link: function(scope, element, attrs) {
-            var fn = $parse(attrs.onReadFile);
-
-      element.on('change', function(onChangeEvent) {
-        var reader = new FileReader();
-
-        reader.onload = function(onLoadEvent) {
-          scope.$apply(function() {
-            fn(scope, {$fileContent:onLoadEvent.target.result});
-          });
-        };
-
-        reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
-      });
-    }
-  };
-});
-
