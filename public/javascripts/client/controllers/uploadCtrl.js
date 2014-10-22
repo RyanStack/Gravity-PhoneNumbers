8e44aaea43c098b8b606478ab9b41f8cbcb762df
Ryan Stack <ryanstack@Ryans-MacBook-Pro-2.local>
2014-10-21 19:30:26 -0700

    removed success on new call

--- public/javascripts/client/controllers/uploadCtrl.js
+++ public/javascripts/client/controllers/uploadCtrl.js
@@ -3,6 +3,7 @@
 App.controller('uploadCtrl', function($scope, $http, NumbersAlgorithm, dataProvider) {
 
   $scope.showContent = function($fileContent){
+        $scope.successU = false;
         $scope.content = $fileContent;
         $scope.brokenDown = $scope.content.split('\n')
         $scope.decoded = NumbersAlgorithm.perform($scope.brokenDown)
